561107126983791485eaa7af0a716cae49601bcf
Klever <stalin.m22@gmail.com>
2026-07-31 08:57:35 -0500

    Agregando las pruebas del proyecto

---
+++ src/test/java/ec/edu/espe/agrosmart/AgrosmartApplicationTests.java
@@ -0,0 +1,12 @@
+package ec.edu.espe.agrosmart;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AgrosmartApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+}
